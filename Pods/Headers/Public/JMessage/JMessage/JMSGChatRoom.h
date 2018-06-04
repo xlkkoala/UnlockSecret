8c6304e54444de35d58c2b7e749e0216693c94a1
xlk <496846998@qq.com>
2018-06-04 11:14:24 +0800

    change

---
+++ Pods/Headers/Public/JMessage/JMessage/JMSGChatRoom.h
@@ -0,0 +1,1 @@
+../../../../JMessage/JMessage.framework/Headers/JMSGChatRoom.h
---
+++ Pods/Target Support Files/Pods-UnlockSecret/Pods-UnlockSecret-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UnlockSecret : NSObject
+@end
+@implementation PodsDummy_Pods_UnlockSecret
+@end
