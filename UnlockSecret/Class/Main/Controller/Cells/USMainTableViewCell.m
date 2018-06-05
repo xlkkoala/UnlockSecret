fb317c1fcf2cf1d50c77513af2291e76c0ccdd12
浪~宇轩 <xxx_koala@163.com>
2018-06-05 18:15:55 +0800

    完成首页。找不同布局

---
+++ UnlockSecret/Class/Main/Controller/Cells/USMainTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  USMainTableViewCell.m
+//  UnlockSecret
+//
+//  Created by 程浪V587 on 2018/6/5.
+//  Copyright © 2018年 com.xlk. All rights reserved.
+//
+
+#import "USMainTableViewCell.h"
+
+@implementation USMainTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
--- UnlockSecret/Helper/Header/Configuration.h
+++ UnlockSecret/Helper/Header/Configuration.h
@@ -32,12 +32,6 @@
 #define US_VERSION [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"]
 
  /*
- // 屏幕宽高
- */
-#define SCREEN_WIDTH    [UIScreen mainScreen].bounds.size.width
-#define SCREEN_HEIGHT   [UIScreen mainScreen].bounds.size.height
-
- /*
  // storyboard
  */
 
